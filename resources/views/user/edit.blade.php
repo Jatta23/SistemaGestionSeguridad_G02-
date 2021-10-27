fd60e5028d98847f4c8d69753d50c59cb2e21ec0
jatta23 <abraham23_martinez@hotmail.com>
2021-10-27 16:18:12 -0600

    Cambios quedados

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('template_title')
-    Editar User
+    Editar Usuario
 @endsection
 
 @section('content')
@@ -13,7 +13,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">Editar User</span>
+                        <span class="card-title">Editar Usuario</span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('users.update', $user->id) }}"  role="form" enctype="multipart/form-data">
