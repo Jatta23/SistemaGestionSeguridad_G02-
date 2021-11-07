a6f25f01c752861b2a832227570254cd11510074
jatta23 <abraham23_martinez@hotmail.com>
2021-11-07 10:33:08 -0600

    Refinamienton del QR

--- resources/views/google2fa/register.blade.php
+++ resources/views/google2fa/register.blade.php
@@ -10,7 +10,7 @@
                 <div class="panel-body" style="text-align: center;">
                     <p>Escanea el código QR mostrado a continuación desde tu aplicación de Google Authenticator. Alternativamente, puedes usar el siguiente código: {{ $secret }}</p>
                     <div>
-                        <img src="{{!! $QR_Image !!}}">
+                        {!! $QR_Image !!}
                     </div>
                     <p>Debes obtener el código desde Google Authenticator al escanear el código, debes hacer esto antes de continuar.</p>
                     <div>
